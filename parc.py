d216838fa8e7b35527315a1300ed68ec2bcd12e3
Dylan <dylan@example.com>
2026-03-10 02:01:02 -0400

    Etape 3: Creer la classe Parc avec ses attributs

--- parc.py
+++ parc.py
@@ -5,29 +5,3 @@ class Parc:
         self.adresse = adresse
         self.capacite = capacite
         self.listeVoitures = []
-    
-    def entrerVoiture(self, voiture):
-        if len(self.listeVoitures) < self.capacite:
-            for v in self.listeVoitures:
-                if v.matricule == voiture.matricule:
-                    print("Erreur: Cette voiture existe deja dans le parc")
-                    return False
-            self.listeVoitures.append(voiture)
-            print("Voiture ajoutee au parc")
-            return True
-        else:
-            print("Erreur: Parc plein")
-            return False
-    
-    def sortirVoiture(self, matricule):
-        for v in self.listeVoitures:
-            if v.matricule == matricule:
-                self.listeVoitures.remove(v)
-                print("Voiture retiree du parc")
-                print("Places libres: " + str(self.calculerNbrPlacesLibres()))
-                return True
-        print("Erreur: Voiture non trouvee")
-        return False
-    
-    def calculerNbrPlacesLibres(self):
-        return self.capacite - len(self.listeVoitures)
