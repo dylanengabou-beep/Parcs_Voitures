a1a153d5a5dd22516d31e8def2a2d6575b1e73f0
Dylan <dylan@example.com>
2026-03-10 02:02:25 -0400

    Etape 9: Entrer des voitures dans le parc

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ parc = Parc(1, "123 Rue Principale", 3)
 v1 = Voiture("ABC123", "Toyota", "Rouge")
 v2 = Voiture("DEF456", "Honda", "Bleu")
 v3 = Voiture("GHI789", "Ford", "Noir")
+
+parc.entrerVoiture(v1)
+parc.entrerVoiture(v2)
+parc.entrerVoiture(v3)
