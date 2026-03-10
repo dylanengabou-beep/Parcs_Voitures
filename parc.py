ceb5c48d2ae5ca8d94f000af58bc47166c44cee0
Dylan <dylan@example.com>
2026-03-10 02:01:12 -0400

    Etape 4: Implementer la methode entrerVoiture

--- parc.py
+++ parc.py
@@ -5,3 +5,16 @@ class Parc:
         self.adresse = adresse
         self.capacite = capacite
         self.listeVoitures = []
+    
+    def entrerVoiture(self, voiture):
+        if len(self.listeVoitures) < self.capacite:
+            for v in self.listeVoitures:
+                if v.matricule == voiture.matricule:
+                    print("Erreur: Cette voiture existe deja dans le parc")
+                    return False
+            self.listeVoitures.append(voiture)
+            print("Voiture ajoutee au parc")
+            return True
+        else:
+            print("Erreur: Parc plein")
+            return False
