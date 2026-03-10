fb0885bf5bab646d771a3e8fdc606e9704c29a96
Dylan <dylan@example.com>
2026-03-10 02:00:52 -0400

    Etape 2: Implementer la methode afficher_informations dans Voiture

--- voiture.py
+++ voiture.py
@@ -4,3 +4,8 @@ class Voiture:
         self.matricule = matricule
         self.marque = marque
         self.couleur = couleur
+    
+    def afficher_informations(self):
+        print("Matricule: " + self.matricule)
+        print("Marque: " + self.marque)
+        print("Couleur: " + self.couleur)
