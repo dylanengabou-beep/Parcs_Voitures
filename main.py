482aecdbaabd7e52aa1b64b8ff56e2d63996e120
Dylan <dylan@example.com>
2026-03-10 02:02:13 -0400

    Etape 8: Creer plusieurs objets de la classe Voiture

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ from voiture import Voiture
 from parc import Parc
 
 parc = Parc(1, "123 Rue Principale", 3)
+
+v1 = Voiture("ABC123", "Toyota", "Rouge")
+v2 = Voiture("DEF456", "Honda", "Bleu")
+v3 = Voiture("GHI789", "Ford", "Noir")
